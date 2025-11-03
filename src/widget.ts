6f8d8c7dd512559edf0dbf1a983b609c7c0c6254
Lingtao <lxie@esri.com>
2025-11-03 12:06:51 -0800

    Add random image and caption endpoint

--- src/widget.ts
+++ src/widget.ts
@@ -1,8 +1,7 @@
-import { Widget } from '@lumino/widgets';
 import { MainAreaWidget } from '@jupyterlab/apputils';
-import {
-  imageIcon,
-} from '@jupyterlab/ui-components';
+import { imageIcon } from '@jupyterlab/ui-components';
+import { Widget } from '@lumino/widgets';
+import { requestAPI } from './request';
 
 class ImageCaptionWidget extends Widget {
   // Initialization
@@ -12,9 +11,41 @@ class ImageCaptionWidget extends Widget {
     // Create and append an HTML <p> (paragraph) tag to our widget's node in
     // the HTML document
     const hello = document.createElement('p');
-    hello.innerHTML = "Hello, world!";
+    hello.innerHTML = 'Hello, world!';
     this.node.appendChild(hello);
+
+    const center = document.createElement('center');
+    this.node.appendChild(center);
+
+    // Put an <img> tag into the <center> tag, and also save it as a class
+    // attribute so we can update it later.
+    this.img = document.createElement('img');
+    center.appendChild(this.img);
+
+    // Do the same for a caption!
+    this.caption = document.createElement('p');
+    center.appendChild(this.caption);
+
+    // Initialize the image from the server extension
+    this.load_image();
   }
+  // Fetch data from the server extension and save the results to img and
+  // caption class attributes
+  load_image(): void {
+    requestAPI<any>('random-image-caption')
+      .then(data => {
+        console.log(data);
+        this.img.src = `data:image/jpeg;base64, ${data.b64_bytes}`;
+        this.caption.innerHTML = data.caption;
+      })
+      .catch(reason => {
+        console.error(`Error fetching image data.\n${reason}`);
+      });
+  }
+
+  // Information about class attributes for the type checker
+  img: HTMLImageElement;
+  caption: HTMLParagraphElement;
 }
 
 export class ImageCaptionMainAreaWidget extends MainAreaWidget<ImageCaptionWidget> {
