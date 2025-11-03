63ced7285bdd3ca49a02bda04f717302544a8860
Lingtao <lxie@esri.com>
2025-11-03 11:34:53 -0800

    Register widget with launcher

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,31 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { ICommandPalette } from '@jupyterlab/apputils';
+import { ILauncher } from '@jupyterlab/launcher';
 
 import { requestAPI } from './request';
+import { ImageCaptionMainAreaWidget } from './widget';
 
 /**
  * Initialization data for the jupytercon2025-extension-workshop extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupytercon2025-extension-workshop:plugin',
-  description: 'A JupyterLab extension that displays a random image and caption.',
+  description:
+    'A JupyterLab extension that displays a random image and caption.',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupytercon2025-extension-workshop is activated!');
+  requires: [ICommandPalette, ILauncher],
+  activate: (
+    app: JupyterFrontEnd,
+    // The activation method receives dependencies in the order they are specified in
+    // the "requires" parameter above:
+    palette: ICommandPalette,
+    launcher: ILauncher
+  ) => {
+    console.log(
+      'JupyterLab extension jupytercon2025-extension-workshop is activated with command palette!'
+    );
 
     requestAPI<any>('hello')
       .then(data => {
@@ -24,6 +37,22 @@ const plugin: JupyterFrontEndPlugin<void> = {
           `The jupytercon2025_extension_workshop server extension appears to be missing.\n${reason}`
         );
       });
+
+    //Register a new command:
+    const command_id = 'image-caption:open';
+    app.commands.addCommand(command_id, {
+      execute: () => {
+        // When the command is executed, create a new instance of our widget
+        const widget = new ImageCaptionMainAreaWidget();
+
+        // Then add it to the main area:
+        app.shell.add(widget, 'main');
+      },
+      label: 'View a random image & caption'
+    });
+
+    palette.addItem({ command: command_id, category: 'Tutorial' });
+    launcher.add({ command: command_id });
   }
 };
 
